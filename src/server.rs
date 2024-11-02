6adc90c60e3cf9e8e3d0f03b88b44c47bff09f51
yuki <2624466175@qq.com>
2024-11-02 14:35:32 +0000

    feat: add tracing

--- src/server.rs
+++ src/server.rs
@@ -4,6 +4,8 @@ use crate::{
     usecase::{category::CategoryUsecase, data::DataUsecase, tag::TagUsecase},
 };
 use axum::Router;
+use tower_http::trace::TraceLayer;
+use tracing::info;
 
 pub struct Server {
     router: Router,
@@ -11,17 +13,23 @@ pub struct Server {
 
 impl Server {
     pub fn new(repo: Repo) -> Self {
-        let router = Router::new().nest(
-            "/api",
-            Router::new()
-                .nest("/tag", tag_router(TagUsecase::new(&repo)))
-                .nest("/category", category_router(CategoryUsecase::new(&repo)))
-                .nest("/data", data_router(DataUsecase::new(&repo))),
-        );
+        tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::TRACE)
+            .init();
+        let router = Router::new()
+            .nest(
+                "/api",
+                Router::new()
+                    .nest("/tag", tag_router(TagUsecase::new(&repo)))
+                    .nest("/category", category_router(CategoryUsecase::new(&repo)))
+                    .nest("/data", data_router(DataUsecase::new(&repo))),
+            )
+            .layer(TraceLayer::new_for_http());
         Server { router }
     }
     pub async fn run(self, port: usize) -> anyhow::Result<()> {
         let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
+        info!("Family estate server listening port: {}", port);
         axum::serve(listener, self.router).await?;
         Ok(())
     }
